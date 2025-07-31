e243b6cb1239a1a3014f4ee6d92a93221d07fd70
theritikkk <ritikraj2004@gmail.com>
2025-07-31 23:58:36 +0530

    feat: implement like toggling functionality for videos, comments, and tweets with appropriate error handling

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,11 +1,25 @@
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { Tweet } from "../models/tweet.model.js"
+import { User } from "../models/user.model.js"
+import mongoose, { isValidObjectId } from "mongoose"
 
 
 const healthcheck = asyncHandler(async (req, res) => {
+    
     //TODO: build a healthcheck response that simply returns the OK status as json with a message
-})
+
+    res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            { message: " Everything is oK! " },
+            "OK"
+        )
+    );
+});
 
 export {
     healthcheck
