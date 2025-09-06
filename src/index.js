7d39f5aef5e628876ebcdd0e6529a569cd7f6ddc
Ritik Raj <158508104+theritikkk@users.noreply.github.com>
2025-09-06 22:10:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ dotenv.config({
 
 connectDB()
 .then( () => {
-    app.listen( process.env.PORT || 80000, () => {
+    app.listen( process.env.PORT || 8000, () => {
         console.log(` Server is runing at port : ${process.env.PORT} `);
     } )
 })
